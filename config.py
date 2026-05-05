6b1111cb94b6974f9b85d6b05ffa22fd9df8c22a
jimbo-b <117157253+jimbo-b@users.noreply.github.com>
2026-05-05 17:47:39 -0400

    Add PostgreSQL configuration and engine setup

---
+++ config.py
@@ -0,0 +1,24 @@
+from sqlalchemy import create_engine
+
+POSTGRES_USER = "postgres"
+POSTGRES_PASSWORD = "password"
+POSTGRES_HOST = "localhost"
+POSTGRES_PORT = 5433
+POSTGRES_ADMIN_DB = "postgres"
+POSTGRES_CREATE_DB = "address_gis"
+
+POSTGRES_ADMIN_URL = (
+    f"postgresql+psycopg2://{POSTGRES_USER}:"
+    f"{POSTGRES_PASSWORD}@{POSTGRES_HOST}:"
+    f"{POSTGRES_PORT}/{POSTGRES_ADMIN_DB}"
+)
+
+admin_engine = create_engine(POSTGRES_ADMIN_URL, isolation_level="AUTOCOMMIT")
+
+CREATE_DB_URL = (
+    f"postgresql+psycopg2://{POSTGRES_USER}:"
+    f"{POSTGRES_PASSWORD}@{POSTGRES_HOST}:"
+    f"{POSTGRES_PORT}/{POSTGRES_CREATE_DB}"
+)
+
+create_db_engine = create_engine(POSTGRES_ADMIN_URL, isolation_level="AUTOCOMMIT")
